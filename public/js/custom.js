351d28e980bd41cd38302cac15662760e9d96430
Arslan Ejaz <arslanejzmalik@gmail.com>
2024-02-09 17:33:47 +0500

    changes

---
+++ public/js/custom.js
@@ -0,0 +1,17 @@
+(function($){
+    "use strict";
+  
+  jQuery(document).ready(function($) {
+
+ 
+  
+
+  
+  });
+
+    function showLoader() {
+        // Show loader overlay
+        document.getElementById('loader-overlay').style.display = 'block';
+    }
+  
+})(jQuery);
