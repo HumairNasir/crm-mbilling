1867edeb9a0dd3c95477bfa7c16a14ba94552010
HumairNasir <humairj165@gmail.com>
2026-02-26 21:11:05 +0500

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
 class UserSeeder extends Seeder
@@ -26,31 +27,44 @@ class UserSeeder extends Seeder
                 'role' => 'CountryManager',
                 'email' => 'countrymanager@kriss.com',
             ],
-            [
-                'name' => 'Regional Manager',
-                'role' => 'RegionalManager',
-                'email' => 'RegionalManager@kriss.com',
-            ],
-            [
-                'name' => 'Area Manager',
-                'role' => 'AreaManager',
-                'email' => 'AreaManager@kriss.com',
-            ],
-            [
-                'name' => 'Sales Representative',
-                'role' => 'SalesRepresentative',
-                'email' => 'SalesRepresentative@kriss.com',
-            ],
+            // [
+            //     'name' => 'Regional Manager',
+            //     'role' => 'RegionalManager',
+            //     'email' => 'RegionalManager@kriss.com',
+            // ],
+            // [
+            //     'name' => 'Area Manager',
+            //     'role' => 'AreaManager',
+            //     'email' => 'AreaManager@kriss.com',
+            // ],
+            // [
+            //     'name' => 'Sales Representative',
+            //     'role' => 'SalesRepresentative',
+            //     'email' => 'SalesRepresentative@kriss.com',
+            // ],
         ];
+        // foreach ($users as $user) {
+        //     $u = User::create([
+        //         'name' => $user['name'],
+        //         'email' => $user['email'],
+        //         'password' => bcrypt('1234321%$#@!'),
+        //     ]);
+        //     if (isset($user['role'])) {
+        //         $role = Role::findByName($user['role']);
+        //         $u->assignRole($role);
+        //     }
+        // }
         foreach ($users as $user) {
-            $u = User::create([
-                'name' => $user['name'],
-                'email' => $user['email'],
-                'password' => bcrypt('1234321%$#@!'),
-            ]);
+            $u = User::updateOrCreate(
+                ['email' => $user['email']], // Condition (check existence)
+                [
+                    'name' => $user['name'], // Used only if creating
+                    'password' => Hash::make('1234321%$#@!'), // Always updates password
+                ],
+            );
+
             if (isset($user['role'])) {
-                $role = Role::findByName($user['role']);
-                $u->assignRole($role);
+                $u->syncRoles([$user['role']]);
             }
         }
     }
